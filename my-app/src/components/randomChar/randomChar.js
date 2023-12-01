5a3d51731207ac73288d5c36f9138b3201fcc16f
gorodnichaya-av <test@m.com>
2023-12-01 14:48:08 +0200

    spinner

--- my-app/src/components/randomChar/randomChar.js
+++ my-app/src/components/randomChar/randomChar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
+import Spinner from '../spinner';
 
 export default class RandomChar extends Component {
 
@@ -11,11 +12,15 @@ export default class RandomChar extends Component {
 
     gotService = new gotService();
     state = {
-        char: {}
+        char: {},
+        loading: true
     }
 
     onCharLoaded = (char) => {
-        this.setState(char);
+        this.setState({
+            char,
+            loading: false
+        });
     }
 
     updateChar() {
@@ -26,27 +31,27 @@ export default class RandomChar extends Component {
 
     render() {
 
-        const {name, gender, born, died, culture} = this.state;
-
+        const {char: {name, gender, born, died, culture}, loading} = this.state;
+        
         return (
             <div className="random-block rounded">
-                <h4>Random Character: {name}</h4>
+                <h4>Random Character: {loading ? <Spinner/> : name}</h4>
                 <ul className="list-group list-group-flush">
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Gender </span>
-                        <span>{gender}</span>
+                        <span>{loading ? <Spinner/> : gender}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Born </span>
-                        <span>{born}</span>
+                        <span>{loading ? <Spinner/> : born}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Died </span>
-                        <span>{died}</span>
+                        <span>{loading ? <Spinner/> : died}</span>
                     </li>
                     <li className="list-group-item d-flex justify-content-between">
                         <span className="term">Culture </span>
-                        <span>{culture}</span>
+                        <span>{loading ? <Spinner/> : culture}</span>
                     </li>
                 </ul>
             </div>
