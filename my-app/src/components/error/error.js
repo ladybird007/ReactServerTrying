f21c54c099b2b5ae4006cfa431b29efbe591d7f0
ishunter216 <michael.king@edenprogram.com>
2023-12-04 12:46:53 +0200

    show image from local SRC folder

--- my-app/src/components/error/error.js
+++ my-app/src/components/error/error.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './error.css';
+import img from './error.jpg';
 
 const ErrorMessage = () => {
    return (
       <>
-         <div className="error-img"><img src={process.env.PUBLIC_URL + '/img/error.jpg'} alt='error'></img></div>
+         <div className="error-img"><img src={img} alt='error'></img></div>
          <span>Error message</span>
       </>
    ) 
