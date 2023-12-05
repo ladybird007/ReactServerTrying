6ad91511c5dc36bfbd354195899e5217482e7d9f
ishunter216 <michael.king@edenprogram.com>
2023-12-05 14:17:46 +0200

    hide/show content on click

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -1,34 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
+import Button from 'react-bootstrap/Button';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+export default class App extends Component {
+    state = {
+        randomChar: true
+    }
 
-export default App;
+    toggleButton = () => {
+        this.setState({randomChar: !this.state.randomChar})
+        /* if (this.state.randomChar) {
+            this.setState({
+                randomChar: false
+            });
+        } else {
+            this.setState({
+                randomChar: true
+            });
+        } */
+    }
+
+    render() {
+        const {randomChar} = this.state;
+        const activeClass = randomChar ? 'primary' : 'outline-primary'; 
+        const content = randomChar ? <RandomChar/> : null;
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}} className='mb-4'>
+                            {content}
+                            <Button variant={activeClass} onClick={()=>this.toggleButton()}>Toggle</Button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
+    
+};
